7435396679709d681133df33eb0d60c627785f33
Viktor H <66110545+vsHonta@users.noreply.github.com>
2022-11-01 01:39:31 +0200

    2-3 tasks done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const refIngredients = document.querySelector('#ingredients');
+
+const allTags = ingredients.map(item => {
+  const tag = document.createElement('li');
+  tag.classList.add('item');
+  tag.textContent = `${item}`;  
+  return tag
+})
+
+refIngredients.append(...allTags)
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,25 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+const refGallery = document.querySelector('.gallery')
+
+const tags = images.reduce((acc, obj) => {
+  const image = `<img src=${obj.url} alt='${obj.alt}' class=img width=300 height=200>`
+  acc += image
+  return acc
+}, ``)
+
+refGallery.insertAdjacentHTML("afterbegin", tags)
+  
+refGallery.style.display = 'flex';
+refGallery.style.gap = '15px';
+refGallery.style.flexDirection = 'column';
+
+
+// const tags = images.map(obj => `<img src=${obj.url} alt=${obj.alt} class=img width=300 height=200> `).join('')
+// refGallery.insertAdjacentHTML("afterbegin", tags)
+
+
+
