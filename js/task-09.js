d03924eb50c3f11ae30ccee5ba567b2d88f16af6
Viktor H <66110545+vsHonta@users.noreply.github.com>
2022-11-03 01:30:50 +0200

    Homework ver-1 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  span: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
+}
+
+const handleClick = evt => {
+  const color = getRandomHexColor();
+  refs.body.style.backgroundColor = `${color}`;
+  refs.span.innerHTML = color
+}
+
+refs.button.addEventListener('click', handleClick)
